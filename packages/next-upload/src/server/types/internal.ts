832e9b01eb99027255ae21bfeb4c731ab4a4d32b
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2024-09-08 17:05:58 -0300

    feat(next-upload): add jsdocs to types

--- packages/next-upload/src/server/types/internal.ts
+++ packages/next-upload/src/server/types/internal.ts
@@ -4,15 +4,41 @@ export type Metadata = Record<string, string>;
 export type OptionalMetadata = Record<string, string | undefined>;
 
 export type UploadedFileInfo = {
+  /**
+   * The name of the file.
+   */
   name: string;
+
+  /**
+   * File size in bytes.
+   */
   size: number;
+
+  /**
+   * The MIME type of the file.
+   */
   type: string;
+
+  /**
+   * The bucket key where the file will be uploaded to.
+   */
   bucketKey: string;
 };
 
 export type ExecRoute = () => Route<any>;
 
 export type Route<M> = {
+  /**
+   * Maximum file size in bytes.
+   *
+   * @default 1024 * 1024 * 5 // 5MB
+   *
+   * @example
+   *
+   * ```ts
+   * maxFileSize: 1024 * 1024 * 10 // 10MB
+   * ```
+   */
   maxFileSize?: number;
 
   /**
@@ -33,21 +59,73 @@ export type Route<M> = {
    * ```
    */
   fileTypes?: string[];
+
+  /**
+   * The number of seconds the upload signed URL is valid for.
+   *
+   * @default 60
+   *
+   * @example
+   *
+   * ```ts
+   * signedUrlExpiresIn: 300 // 5 minutes
+   * ```
+   */
   signedUrlExpiresIn?: number;
 
+  /**
+   * Use this callback to run custom logic before uploading a file, such as auth and rate-limiting. You can also return a custom bucket key.
+   *
+   * Metadata sent from the client is also available.
+   *
+   * Throw an `UploadFileError` to reject the file upload. This will also send the error message to the client.
+   */
   onBeforeUpload?: (data: {
+    /**
+     * The incoming request from Next.js.
+     */
     req: NextRequest;
+
+    /**
+     * Information about the file to be uploaded.
+     */
     file: Omit<UploadedFileInfo, 'bucketKey'>;
+
+    /**
+     * Metadata sent from the client.
+     */
     clientMetadata: OptionalMetadata;
   }) =>
     | { bucketKey?: string; metadata?: M }
     | void
     | Promise<{ bucketKey?: string; metadata?: M } | void>;
 
+  /**
+   * Use this callback to run custom logic after creating the signed URL, such as saving data to a database.
+   *
+   * Metadata sent from `onBeforeUpload` is available as `metadata`. Metadata sent from the client is available as `clientMetadata`.
+   *
+   * You can return additional metadata to be sent back to the client.
+   */
   onAfterSignedUrl?: (data: {
+    /**
+     * The incoming request from Next.js.
+     */
     req: NextRequest;
+
+    /**
+     * Information about the uploaded file, including the bucket key.
+     */
     file: UploadedFileInfo;
+
+    /**
+     * Metadata sent from `onBeforeUpload`.
+     */
     metadata: M;
+
+    /**
+     * Metadata sent from the client.
+     */
     clientMetadata: OptionalMetadata;
   }) =>
     | { metadata?: Record<string, string> }
--- packages/next-upload/src/server/types/public.ts
+++ packages/next-upload/src/server/types/public.ts
@@ -2,8 +2,19 @@ import type { S3Client } from '@aws-sdk/client-s3';
 import type { ExecRoute } from './internal';
 
 export type Router = {
+  /**
+   * The S3 client.
+   */
   client: S3Client;
+
+  /**
+   * The name of the bucket where the files will be uploaded to.
+   */
   bucketName: string;
+
+  /**
+   * The routes where files can be uploaded to.
+   */
   routes: {
     [key: string]: ExecRoute;
   };
