76f53581e08824ed53e481cbabe5de7d237441ba
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2024-09-10 20:07:54 -0300

    feat(next-upload): base for server helpers

--- packages/next-upload/tsup.config.ts
+++ packages/next-upload/tsup.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/server/index.ts', 'src/client/index.ts'],
+  entry: [
+    'src/server/index.ts',
+    'src/server/helpers/index.ts',
+    'src/client/index.ts',
+  ],
   format: ['cjs', 'esm'],
   dts: true,
   outDir: 'dist',
