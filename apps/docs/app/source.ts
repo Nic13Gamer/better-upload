aea3260cdcdc9faa4916895e922c5466e211fd69
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2024-09-22 20:07:38 -0300

    feat(docs): add icons to docs

--- apps/docs/app/source.ts
+++ apps/docs/app/source.ts
@@ -1,8 +1,15 @@
 import { docs, meta } from '@/.source';
+import { create } from '@/components/ui/icon';
 import { loader } from 'fumadocs-core/source';
 import { createMDXSource } from 'fumadocs-mdx';
+import { icons } from 'lucide-react';
 
 export const source = loader({
   baseUrl: '/',
   source: createMDXSource(docs, meta),
+  icon(icon) {
+    if (icon && icon in icons) {
+      return create({ icon: icons[icon as keyof typeof icons] });
+    }
+  },
 });
