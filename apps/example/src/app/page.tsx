259db2abdc285dff7f2d940dc667d12bc259aeaa
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2024-10-13 20:37:21 -0300

    style(example): components text

--- apps/example/src/app/page.tsx
+++ apps/example/src/app/page.tsx
@@ -8,13 +8,15 @@ export default function Home() {
         <h1 className="text-2xl font-semibold">Next.js File Upload Example</h1>
 
         <div className="w-full space-y-1">
-          <p className="text-sm font-medium">Single file (template)</p>
+          <p className="text-sm font-medium">Single file (upload button)</p>
 
           <FileUploader />
         </div>
 
         <div className="w-full space-y-1">
-          <p className="text-sm font-medium">Multiple files</p>
+          <p className="text-sm font-medium">
+            Multiple files (upload dropzone)
+          </p>
 
           <FilesUploader />
         </div>
