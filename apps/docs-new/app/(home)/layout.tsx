f5944e8396fcd396a5cc3bfd34c0b05850df0bb5
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2025-07-23 19:49:35 -0300

    docs: home page & improvements

--- apps/docs-new/app/(home)/layout.tsx
+++ apps/docs-new/app/(home)/layout.tsx
@@ -3,5 +3,12 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import type { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return (
+    <HomeLayout
+      {...baseOptions}
+      links={[{ text: 'Documentation', url: '/docs' }]}
+    >
+      {children}
+    </HomeLayout>
+  );
 }
--- apps/docs-new/app/sitemap.ts
+++ apps/docs-new/app/sitemap.ts
@@ -13,14 +13,17 @@ const url = (path: string): string => new URL(path, baseUrl).toString();
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
+    {
+      url: url('/'),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
     ...(await Promise.all(
       source.getPages().map(async (page) => {
-        const priority = page.url === '/' ? 1 : 0.5;
-
         return {
           url: url(page.url),
           changeFrequency: 'weekly',
-          priority,
+          priority: 0.9,
         } as MetadataRoute.Sitemap[number];
       })
     )),
