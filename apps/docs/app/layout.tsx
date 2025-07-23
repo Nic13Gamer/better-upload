22bc2b544317234ce9e84e122272f38a951f2e30
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2025-07-23 20:02:15 -0300

    docs(v0): add version 0 banner

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,4 +1,5 @@
 import { source } from '@/lib/source';
+import { Banner } from 'fumadocs-ui/components/banner';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Metadata } from 'next';
@@ -6,6 +7,8 @@ import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 import { baseOptions } from './layout.config';
 
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
 import './global.css';
 
 const inter = Inter({
@@ -33,6 +36,17 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body>
+        <Banner className="text-fd-muted-foreground gap-2">
+          You are viewing the docs for version 0.{' '}
+          <Link
+            href="https://better-upload.com"
+            className="text-fd-foreground flex items-center gap-1.5 underline"
+          >
+            Switch to latest
+            <ArrowRight className="size-3.5" />
+          </Link>
+        </Banner>
+
         <RootProvider search={{ options: { type: 'static' } }}>
           <DocsLayout tree={source.pageTree} {...baseOptions}>
             {children}
