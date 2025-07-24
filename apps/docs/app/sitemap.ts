65e0ac570d075f6c72f8450a70042e40cb3962e8
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2025-07-23 21:25:23 -0300

    chore: update CI & docs sitemap

--- apps/docs/app/sitemap.ts
+++ apps/docs/app/sitemap.ts
@@ -4,10 +4,9 @@ import { MetadataRoute } from 'next';
 export const revalidate = false;
 
 const baseUrl =
-  process.env.NODE_ENV === 'development' ||
-  !process.env.VERCEL_PROJECT_PRODUCTION_URL
+  process.env.NODE_ENV === 'development'
     ? new URL('http://localhost:3000')
-    : new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`);
+    : new URL('https://better-upload.com');
 
 const url = (path: string): string => new URL(path, baseUrl).toString();
 
