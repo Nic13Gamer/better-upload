e74c99fc8bd45f32518a3868bdcf308e2218cc4e
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2025-09-27 15:24:48 -0300

    docs: add toc footer banner

--- apps/docs/app/docs/[[...slug]]/page.tsx
+++ apps/docs/app/docs/[[...slug]]/page.tsx
@@ -1,3 +1,4 @@
+import { TocFooter } from '@/components/toc-footer';
 import { source } from '@/lib/source';
 import { getMDXComponents } from '@/mdx-components';
 import { createRelativeLink } from 'fumadocs-ui/mdx';
@@ -21,7 +22,11 @@ export default async function Page(props: {
   const MDXContent = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage
+      toc={page.data.toc}
+      full={page.data.full}
+      tableOfContent={{ footer: <TocFooter /> }}
+    >
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
 
