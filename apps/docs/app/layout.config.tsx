eed849cec1c254c5b92161d7db98bc801bdddd79
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2025-10-10 12:15:16 -0300

    docs: X social icon

--- apps/docs/app/layout.config.tsx
+++ apps/docs/app/layout.config.tsx
@@ -1,9 +1,22 @@
+import { XIcon } from '@/components/icons';
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: 'Better Upload',
   },
-  links: [],
+  links: [
+    {
+      url: 'https://x.com/Nic13Gamer',
+      text: 'X',
+      type: 'icon',
+      icon: (
+        <div className="scale-95">
+          <XIcon />
+        </div>
+      ),
+      external: true,
+    },
+  ],
   githubUrl: 'https://github.com/Nic13Gamer/better-upload',
 };
