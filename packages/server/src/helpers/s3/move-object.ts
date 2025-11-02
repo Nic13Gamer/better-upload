f71b7723adef41d7a70824a4c29831cf0acddba0
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2025-11-02 12:10:12 -0300

    refactor: move object helper

--- packages/server/src/helpers/s3/move-object.ts
+++ packages/server/src/helpers/s3/move-object.ts
@@ -1,48 +1,28 @@
 import type { Client } from '@/types/clients';
-import { throwS3Error } from '@/utils/s3';
+import { copyObject } from './copy-object';
+import { deleteObject } from './delete-object';
 
 /**
- * Move an object from one key to another inside an S3 bucket.
+ * Move an object from one key to another, within or between, S3 buckets.
  *
  * **WARNING:** This copies the object to the new key and then deletes the original object. It can be slow.
  */
 export async function moveObject(
   client: Client,
   params: {
-    bucket: string;
-
-    /**
-     * The key of the object to move. Do not include the bucket name.
-     *
-     * @example 'example.jpg'
-     */
-    fromKey: string;
-
-    /**
-     * The key of where the object will be moved to. Do not include the bucket name.
-     *
-     * @example 'images/example.jpg'
-     */
-    toKey: string;
+    source: {
+      bucket: string;
+      key: string;
+    };
+    destination: {
+      bucket: string;
+      key: string;
+    };
   }
 ) {
-  await throwS3Error(
-    client.s3.fetch(`${client.buildBucketUrl(params.bucket)}/${params.toKey}`, {
-      method: 'PUT',
-      headers: {
-        'x-amz-copy-source': `${params.bucket}/${params.fromKey}`,
-      },
-      aws: { signQuery: true, allHeaders: true },
-    })
-  );
-
-  await throwS3Error(
-    client.s3.fetch(
-      `${client.buildBucketUrl(params.bucket)}/${params.fromKey}`,
-      {
-        method: 'DELETE',
-        aws: { signQuery: true, allHeaders: true },
-      }
-    )
-  );
+  await copyObject(client, params);
+  await deleteObject(client, {
+    bucket: params.source.bucket,
+    key: params.source.key,
+  });
 }
