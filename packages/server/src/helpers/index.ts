4fa8ac203baaa31659b96ecdf2f73a98774dcd78
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2025-11-18 19:17:33 -0300

    feat: putObject helper

--- packages/server/src/helpers/index.ts
+++ packages/server/src/helpers/index.ts
@@ -3,5 +3,6 @@ export * from './s3/delete-object';
 export * from './s3/get-object';
 export * from './s3/head-object';
 export * from './s3/move-object';
+export * from './s3/put-object';
 
 export * from './s3/presign/get-object';
